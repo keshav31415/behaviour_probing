ae57997a17c0e1e719c0fc489490611c84270123
keshav31415 <keshavkaushiksps@gmail.com>
2026-09-01 16:22:16 +0530

    Optimize training evaluation with batched inference

--- utils.py
+++ utils.py
@@ -127,57 +127,56 @@ def data_partition(fname):
 # TODO: merge evaluate functions for test and val set
 # evaluate on test set
 def evaluate(model, dataset, args):
+    import torch
     [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
     NDCG = 0.0
     HT = 0.0
     valid_user = 0.0
-
     if usernum>10000:
         users = random.sample(range(1, usernum + 1), 10000)
     else:
         users = range(1, usernum + 1)
-    for u in users:
-
-        if len(train[u]) < 1 or len(test[u]) < 1: continue
-
-        seq = np.zeros([args.maxlen], dtype=np.int32)
-        idx = args.maxlen - 1
-        seq[idx] = valid[u][0]
-        idx -= 1
-        for i in reversed(train[u]):
-            seq[idx] = i
-            idx -= 1
-            if idx == -1: break
-        rated = set(train[u])
-        rated.add(0)
-        item_idx = [test[u][0]]
-        for _ in range(100):
-            t = np.random.randint(1, itemnum + 1)
-            while t in rated: t = np.random.randint(1, itemnum + 1)
-            item_idx.append(t)
-
-        predictions = -model.predict(*[np.array(l) for l in [[u], [seq], item_idx]])
-        predictions = predictions[0] # - for 1st argsort DESC
-
-        rank = predictions.argsort().argsort()[0].item()
-
-        valid_user += 1
-
-        if rank < 10:
-            NDCG += 1 / np.log2(rank + 2)
-            HT += 1
-        if valid_user % 100 == 0:
-            print('.', end="")
-            sys.stdout.flush()
-
+    eval_users = [u for u in users if len(train.get(u, [])) >= 1 and len(test.get(u, [])) >= 1]
+    batch_size = 256
+    with torch.no_grad():
+        for i in range(0, len(eval_users), batch_size):
+            batch_u = eval_users[i:i+batch_size]
+            batch_seqs, batch_item_idx = [], []
+            for u in batch_u:
+                seq = np.zeros([args.maxlen], dtype=np.int32)
+                idx = args.maxlen - 1
+                seq[idx] = valid[u][0]
+                idx -= 1
+                for item in reversed(train[u]):
+                    seq[idx] = item
+                    idx -= 1
+                    if idx == -1: break
+                batch_seqs.append(seq)
+                rated = set(train[u])
+                rated.add(0)
+                item_idx = [test[u][0]]
+                for _ in range(100):
+                    t = np.random.randint(1, itemnum + 1)
+                    while t in rated: t = np.random.randint(1, itemnum + 1)
+                    item_idx.append(t)
+                batch_item_idx.append(item_idx)
+            predictions = -model.predict(np.array(batch_u), np.array(batch_seqs), np.array(batch_item_idx)).cpu().numpy()
+            for b_idx in range(len(batch_u)):
+                rank = predictions[b_idx].argsort().argsort()[0].item()
+                valid_user += 1
+                if rank < 10:
+                    NDCG += 1 / np.log2(rank + 2)
+                    HT += 1
+                if valid_user % 100 == 0:
+                    print(".", end="")
+                    sys.stdout.flush()
     return NDCG / valid_user, HT / valid_user
 
 
 # evaluate on val set
 def evaluate_valid(model, dataset, args):
+    import torch
     [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
     NDCG = 0.0
     valid_user = 0.0
     HT = 0.0
@@ -185,36 +184,36 @@ def evaluate_valid(model, dataset, args):
         users = random.sample(range(1, usernum + 1), 10000)
     else:
         users = range(1, usernum + 1)
-    for u in users:
-        if len(train[u]) < 1 or len(valid[u]) < 1: continue
-
-        seq = np.zeros([args.maxlen], dtype=np.int32)
-        idx = args.maxlen - 1
-        for i in reversed(train[u]):
-            seq[idx] = i
-            idx -= 1
-            if idx == -1: break
-
-        rated = set(train[u])
-        rated.add(0)
-        item_idx = [valid[u][0]]
-        for _ in range(100):
-            t = np.random.randint(1, itemnum + 1)
-            while t in rated: t = np.random.randint(1, itemnum + 1)
-            item_idx.append(t)
-
-        predictions = -model.predict(*[np.array(l) for l in [[u], [seq], item_idx]])
-        predictions = predictions[0]
-
-        rank = predictions.argsort().argsort()[0].item()
-
-        valid_user += 1
-
-        if rank < 10:
-            NDCG += 1 / np.log2(rank + 2)
-            HT += 1
-        if valid_user % 100 == 0:
-            print('.', end="")
-            sys.stdout.flush()
-
+    eval_users = [u for u in users if len(train.get(u, [])) >= 1 and len(valid.get(u, [])) >= 1]
+    batch_size = 256
+    with torch.no_grad():
+        for i in range(0, len(eval_users), batch_size):
+            batch_u = eval_users[i:i+batch_size]
+            batch_seqs, batch_item_idx = [], []
+            for u in batch_u:
+                seq = np.zeros([args.maxlen], dtype=np.int32)
+                idx = args.maxlen - 1
+                for item in reversed(train[u]):
+                    seq[idx] = item
+                    idx -= 1
+                    if idx == -1: break
+                batch_seqs.append(seq)
+                rated = set(train[u])
+                rated.add(0)
+                item_idx = [valid[u][0]]
+                for _ in range(100):
+                    t = np.random.randint(1, itemnum + 1)
+                    while t in rated: t = np.random.randint(1, itemnum + 1)
+                    item_idx.append(t)
+                batch_item_idx.append(item_idx)
+            predictions = -model.predict(np.array(batch_u), np.array(batch_seqs), np.array(batch_item_idx)).cpu().numpy()
+            for b_idx in range(len(batch_u)):
+                rank = predictions[b_idx].argsort().argsort()[0].item()
+                valid_user += 1
+                if rank < 10:
+                    NDCG += 1 / np.log2(rank + 2)
+                    HT += 1
+                if valid_user % 100 == 0:
+                    print(".", end="")
+                    sys.stdout.flush()
     return NDCG / valid_user, HT / valid_user
